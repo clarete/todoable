059e0517e07fd99bbfbf872360c504d3a400764c
Lincoln de Sousa <lincoln@comum.org>
2017-11-22 11:02:02 -0500

    Create Todoable class with method authenticate!

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -1,5 +1,32 @@
 require "todoable/version"
+require 'net/http'
 
 module Todoable
-  # Your code goes here...
+  AUTH_PATH = "/api/authenticate"
+
+  class Todoable
+    def initialize(user, password)
+      @user = user
+      @password = password
+      @token = nil
+    end
+
+    def api_base_uri
+      "http://todoable.teachable.tech"
+    end
+
+    def api_uri(path)
+      URI.join(api_base_uri, path)
+    end
+
+    def authenticate!
+      uri = api_uri(AUTH_PATH)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.basic_auth(@user, @password)
+      request.initialize_http_header({})
+      request['Accept'] = request['Content-Type'] = 'application/json'
+      http.request(request)
+    end
+  end
 end
--- spec/todoable_spec.rb
+++ spec/todoable_spec.rb
@@ -1,9 +1,37 @@
-RSpec.describe Todoable do
+RSpec.describe "Test test setup" do
   it "has a version number" do
     expect(Todoable::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe "Build Endpoints" do
+  it "has an API endpoint accessor" do
+    todoable = Todoable::Todoable.new("user", "password")
+    expect(todoable.api_base_uri).to eql("http://todoable.teachable.tech")
+  end
+
+  it "builds URIs for different API methods" do
+    todoable = Todoable::Todoable.new("user", "password")
+    expect(todoable.api_uri(Todoable::AUTH_PATH)).to(
+      eql(URI("http://todoable.teachable.tech/api/authenticate")))
+  end
+end
+
+RSpec.describe "Authentication" do
+  it "should be able to authenticate" do
+    # Given an instance of the API wrapper
+    todoable = Todoable::Todoable.new("user", "password")
+
+    # And given that the endpoint is stubbed
+    stub_request(:post, todoable.api_uri(Todoable::AUTH_PATH))
+      .with(headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'})
+
+    # When the client tries to authenticate
+    todoable.authenticate!
+
+    # Then it should retrieve the auth token
+    expect(WebMock).to(
+      have_requested(:post, todoable.api_uri(Todoable::AUTH_PATH))
+        .with(headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}))
   end
 end
