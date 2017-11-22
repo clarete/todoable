e09a5bc573845b671d0a238467c6a943c7c0f128
Lincoln de Sousa <lincoln@comum.org>
2017-11-22 16:13:36 -0500

    Implement Todoable.{lists,new_list} & List.update

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -1,8 +1,29 @@
 require "todoable/version"
-require 'net/http'
+require "net/http"
+require "json"
 
 module Todoable
   AUTH_PATH = "/api/authenticate"
+  LISTS_PATH = "/api/lists"
+  LIST_PATH = "/api/lists/:list_id"
+
+  class List
+    attr_accessor :name
+    attr_accessor :src
+    attr_accessor :id
+
+    def initialize(todoable, params)
+      @todoable = todoable
+      @name = params['name']
+      @src = params['src']
+      @id = params['id']
+    end
+
+    def update(name)
+      body = {"list": {"name": name}}.to_json
+      @todoable.request!("patch", LIST_PATH, { :list_id => @id }, body)
+    end
+  end
 
   class Todoable
     def initialize(user, password)
@@ -11,22 +32,58 @@ module Todoable
       @token = nil
     end
 
+    def lists
+      request!("get", LISTS_PATH).collect {|json_list| List.new(self, json_list) }
+    end
+
+    def new_list(name)
+      request!("post", LISTS_PATH, nil, {"list": {"name": name}}.to_json)
+    end
+
     def api_base_uri
       "http://todoable.teachable.tech"
     end
 
-    def api_uri(path)
-      URI.join(api_base_uri, path)
+    def api_uri(path, params = nil)
+      p = path
+      params.each {|k, v| p = p.gsub(/(:#{k.to_s})/, v.to_s)} if params
+      URI.join(api_base_uri, p)
+    end
+
+    def request!(method, uri, params = nil, body = nil)
+      authenticate!
+      uri = api_uri(uri, params)
+      # Choose method
+      request = {
+        "get" => Net::HTTP::Get,
+        "post" => Net::HTTP::Post,
+        "patch" => Net::HTTP::Patch,
+        "delete" => Net::HTTP::Delete,
+      }[method].new(uri.request_uri)
+
+      # Configure request object
+      request.body = body
+      request.initialize_http_header({
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' => "Token token=\"#{@token}\""
+      })
+
+      # Perform request and collect response
+      http = Net::HTTP.new(uri.host, uri.port)
+      body = http.request(request).body
+      JSON.parse(body) if !body.empty?
     end
 
     def authenticate!
+      return unless @token == nil
       uri = api_uri(AUTH_PATH)
-      http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post.new(uri.request_uri)
       request.basic_auth(@user, @password)
       request.initialize_http_header({})
       request['Accept'] = request['Content-Type'] = 'application/json'
-      http.request(request)
+      http = Net::HTTP.new(uri.host, uri.port)
+      @token = JSON.parse(http.request(request).body)["token"]
     end
   end
 end
--- spec/todoable_spec.rb
+++ spec/todoable_spec.rb
@@ -15,16 +15,33 @@ RSpec.describe "Build Endpoints" do
     expect(todoable.api_uri(Todoable::AUTH_PATH)).to(
       eql(URI("http://todoable.teachable.tech/api/authenticate")))
   end
+
+  it "builds endpoints with parameters" do
+    todoable = Todoable::Todoable.new("user", "password")
+    expect(todoable.api_uri(Todoable::LIST_PATH, {:list_id => 10})).to(
+      eql(URI("http://todoable.teachable.tech/api/lists/10")))
+  end
+end
+
+def default_headers
+  {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+end
+
+def auth_headers(token)
+  {'Accept' => 'application/json',
+   'Content-Type' => 'application/json',
+   'Authorization' => "Token token=\"#{token}\""}
 end
 
 RSpec.describe "Authentication" do
-  it "should be able to authenticate" do
+  it "access the authentication end point" do
     # Given an instance of the API wrapper
     todoable = Todoable::Todoable.new("user", "password")
 
     # And given that the endpoint is stubbed
     stub_request(:post, todoable.api_uri(Todoable::AUTH_PATH))
-      .with(headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'})
+      .with(headers: default_headers)
+      .to_return(body: '{ "token": "foo" }')
 
     # When the client tries to authenticate
     todoable.authenticate!
@@ -32,6 +49,118 @@ RSpec.describe "Authentication" do
     # Then it should retrieve the auth token
     expect(WebMock).to(
       have_requested(:post, todoable.api_uri(Todoable::AUTH_PATH))
-        .with(headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'}))
+        .with(headers: default_headers))
+  end
+
+  it "use the authentication token in other requests" do
+    # Given an instance of the API wrapper
+    todoable = Todoable::Todoable.new("user", "password")
+
+    # And given that the authentication endpoint is stubbed
+    stub_request(:post, todoable.api_uri(Todoable::AUTH_PATH))
+      .with(headers: default_headers)
+      .to_return(body: '{ "token": "Very Secret Token" }')
+
+    # And given that the lists endpoint is also stubbed
+    stub_request(:get, todoable.api_uri(Todoable::LISTS_PATH))
+      .with(headers: auth_headers('Very Secret Token'))
+      .to_return(body: '[]')
+
+    # When it tries to retrieve lists for example
+    todoable.lists()
+
+    # Then it should hit the API to retrieve the lists
+    expect(WebMock).to(
+      have_requested(:get, todoable.api_uri(Todoable::LISTS_PATH))
+        .with(headers: auth_headers('Very Secret Token')))
+  end
+end
+
+
+RSpec.describe "Lists" do
+  before do
+    # Instance that will be the target of all these tests
+    @todoable = Todoable::Todoable.new("user", "password")
+
+    # Stub the request to the auth endpoint that is needed by all
+    # other tests in this class
+    stub_request(:post, @todoable.api_uri(Todoable::AUTH_PATH))
+      .with(headers: default_headers)
+      .to_return(body: '{ "token": "Very Secret Token" }')
+  end
+
+  it "works with no lists" do
+    # Given that the lists endpoint is stubbed to return no lists
+    stub_request(:get, @todoable.api_uri(Todoable::LISTS_PATH))
+      .with(headers: auth_headers('Very Secret Token'))
+      .to_return(body: '[]')
+
+    # When it tries to retrieve lists
+    lists = @todoable.lists()
+
+    # Then it should return just an empty list
+    expect(lists).to eql([])
+  end
+
+  it "retrieve lists" do
+    # Given that the lists endpoint is stubbed to return a single list
+    stub_request(:get, @todoable.api_uri(Todoable::LISTS_PATH))
+      .with(headers: auth_headers('Very Secret Token'))
+      .to_return(body: '[{' \
+                       '  "name": "Urgent Things",' \
+                       '  "src":  "http://todoable.teachable.tech/api/lists/:list_id",' \
+                       '  "id":  44' \
+                       '}]')
+
+    # When it tries to retrieve lists
+    lists = @todoable.lists()
+
+    # Then it should return just an empty list
+    expect(lists.length).to eql(1)
+    expect(lists[0]).to be_instance_of(Todoable::List)
+    expect(lists[0].name).to eql("Urgent Things")
+    expect(lists[0].src).to eql("http://todoable.teachable.tech/api/lists/:list_id")
+    expect(lists[0].id).to eql(44)
+  end
+
+  it "create lists" do
+    # Given that the method POST of the lists endpoint is stubbed like
+    # the following:
+    stub_request(:post, @todoable.api_uri(Todoable::LISTS_PATH))
+      .with(headers: auth_headers('Very Secret Token'))
+      .to_return(status: 200)
+
+    # When a new list is added to the todo instance
+    @todoable.new_list("Fun things")
+
+    # Then the post should have been performed against the lists path
+    expect(WebMock).to(
+      have_requested(:post, @todoable.api_uri(Todoable::LISTS_PATH))
+        .with(headers: auth_headers('Very Secret Token'),
+              body: {"list": {"name": "Fun things"}}.to_json))
+  end
+
+  it "update a list" do
+    # Given a list
+    list = Todoable::List.new @todoable, {
+      'name' => 'foo',
+      'src' => @todoable.api_uri(Todoable::LIST_PATH, {:list_id => 42}),
+      'id' => 42
+    }
+
+    # And given that the method PATCH of the lists endpoint is stubbed
+    # like the following:
+    stub_request(:patch, list.src)
+      .with(headers: auth_headers('Very Secret Token'))
+      .to_return(status: 200)
+
+    # When the list is updated
+    list.update("bar")
+
+    # Then it's expected
+    expect(WebMock).to(
+      have_requested(:patch, list.src).with(
+        headers: auth_headers('Very Secret Token'),
+        body: {"list": {"name": "bar"}}.to_json))
   end
 end
