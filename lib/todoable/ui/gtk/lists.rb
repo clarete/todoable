ab674de551a8543d26e5fd04efe681c102febbf5
Lincoln de Sousa <lincoln@comum.org>
2017-11-25 20:40:52 -0500

    ui/gtk3: Enumerate lists on the UI after login

--- lib/todoable/ui/gtk/lists.rb
+++ lib/todoable/ui/gtk/lists.rb
@@ -1,22 +1,69 @@
+require 'thread'
 require 'gtk3'
+
+require 'todoable/ui/gtk/toolbar'
+
+class ListsBox < Gtk::Box
+  def initialize mainpanel
+    super :vertical
+    set_margin 10
+    @mainpanel = mainpanel
+
+    # Populate widget with internal UI elements
+    @listview = nil
+    add_ui_elements
+  end
+
+  def load_lists
+    start_loading
+    Thread.new do
+      @mainpanel.todoable.lists.each do |list|
+        @mainpanel.jobqueue.push { @listview.append list.id, list.name }
+      end
+
+      # Our job here's done, let's make the treeview sensitive again
+      # so the user can interact with it
+      @mainpanel.jobqueue.push { finish_loading }
+    end
+  end
+
+  private
+
+  def start_loading
+    set_sensitive false
+    @listview.clear
+  end
+
+  def finish_loading
+    set_sensitive true
+  end
+
+  def add_ui_elements
+    # Add the toolbar
+    pack_start Toolbar.new "Lists", @mainpanel
+
+    # Add the list view
+    @listview = ListsTreeView.new
+    pack_start @listview, :expand => true, :fill => true
+  end
+end
 
 class ListsTreeView < Gtk::TreeView
   COLUMN_ID = 0
   COLUMN_NAME = 1
 
   def initialize
-    @model = Gtk::ListStore.new Integer, String
+    @model = Gtk::ListStore.new String, String
     super @model
-
     setup_columns
-    append 10, "Urgent Things"
   end
 
   def append id, name
     @model.append.set_values [id, name]
   end
 
-  def clean
+  def clear
+    @model.clear
   end
 
   private
--- lib/todoable/ui/gtk/mainpanel.rb
+++ lib/todoable/ui/gtk/mainpanel.rb
@@ -6,6 +6,9 @@ require 'todoable/ui/gtk/lists'
 require 'todoable/ui/gtk/jobqueue'
 
 class MainPanel < Gtk::Stack
+  attr_accessor :jobqueue
+  attr_accessor :todoable
+
   def initialize
     super
 
@@ -24,8 +27,13 @@ class MainPanel < Gtk::Stack
   private
 
   def add_ui_elements
-    # Add the login form and define the login action
+    # Create the elements
     login_form = LoginForm.new
+    add_named login_form, "login_form"
+    list_box = ListsBox.new self
+    add_named list_box, "lists_box"
+
+    # Define the login action
     login_form.signal_connect("connect_bt_clicked") { |w, username, password|
       Thread.new do
         begin
@@ -33,6 +41,7 @@ class MainPanel < Gtk::Stack
           @jobqueue.push {
             set_visible_child "lists_box"
             w.success
+            list_box.load_lists
           }
         rescue Todoable::AuthError
           @jobqueue.push { w.report_error "User or password doesn't match" }
@@ -41,51 +50,5 @@ class MainPanel < Gtk::Stack
         end
       end
     }
-    add_named login_form, "login_form"
-
-    # Add the lists tree view
-    lists_box = Gtk::Box.new :vertical
-    lists_box.set_margin 10
-    lists_box.pack_start Toolbar.new "Lists", self
-    lists_box.pack_start ListsTreeView.new, :expand => true, :fill => true
-    add_named lists_box, "lists_box"
-  end
-end
-
-class Toolbar < Gtk::Box
-  def initialize title, mainpanel
-    super :horizontal, 10
-    @mainpanel = mainpanel
-    add_ui_elements title
-  end
-
-  private
-
-  def add_ui_elements title
-    @label = Gtk::Label.new
-    @label.set_markup "<span font-weight='heavy' font='32'>#{title}</span>"
-    @label.set_alignment 0, 0.5
-    pack_start @label, :expand => true, :fill => true
-
-    # Buttons that always show up
-    bt_disconnect = Gtk::ToolButton.new(
-     :label => "Disconnect",
-     :stock_id => Gtk::Stock::DISCONNECT)
-    bt_disconnect.set_tooltip_text bt_disconnect.label
-    bt_disconnect.signal_connect("clicked") { @mainpanel.set_visible_child "login_form" }
-
-    bt_quit = Gtk::ToolButton.new(
-     :label => "Quit",
-     :stock_id => Gtk::Stock::QUIT)
-    bt_quit.set_tooltip_text bt_quit.label
-    bt_quit.signal_connect("clicked") { Gtk.main_quit }
-
-    # Create the toolbar and add it to the box
-    toolbar = Gtk::Toolbar.new
-    toolbar.style_context.add_class("primary-toolbar")
-    toolbar.insert Gtk::SeparatorToolItem.new.set_draw(false).set_expand(true), 0
-    toolbar.insert bt_disconnect, 1
-    toolbar.insert bt_quit, 2
-    pack_start toolbar
   end
 end
